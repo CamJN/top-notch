be4c4ce343b52b5c689138f9b26ed562df1dc5f9
Camden Narzt <c.narzt@me.com>
2025-09-10 11:47:42 -0600

    make everything pretty

--- Sources/main.swift
+++ Sources/main.swift
@@ -4,14 +4,19 @@ extension NSScreen {
     var hasTopNotchDesign: Bool {
         return safeAreaInsets.top != 0
     }
+    static var hasNotchedScreen: Bool {
+        return screens.contains { $0.hasTopNotchDesign }
+    }
 }
+
+let NO_SCREENS = EXIT_FAILURE + 1
 
 @main
 struct TopNotchTool {
     static func main() {
-        if NSScreen.screens.count < 1 {
-            exit(2)
-        } else if NSScreen.screens.contains(where: { $0.hasTopNotchDesign }) {
+        if NSScreen.screens.isEmpty {
+            exit(NO_SCREENS)
+        } else if NSScreen.hasNotchedScreen {
             exit(EXIT_SUCCESS)
         } else {
             exit(EXIT_FAILURE)
